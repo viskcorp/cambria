cdf1eaac4c73a42cd4826e812ff7c07493a5e12f
viskcorp <viskcorp@gmail.com>
2015-03-17 15:32:31 +0700

    Create vc10.js

---
+++ vc10.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+var home_page=&quot;/&quot;;var urlactivepage=location.href;var postperpage=5;var numshowpage=7;var upPageWord=&#39;&#9668;&#39;;var downPageWord=&#39;&#9658;&#39;;
+//]]>
+</script>
