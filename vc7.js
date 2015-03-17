bcb48de6333e296fc244a57157c97da7d6a6bb2f
viskcorp <viskcorp@gmail.com>
2015-03-17 15:37:18 +0700

    Create vc7.js

---
+++ vc7.js
@@ -0,0 +1,1 @@
+<script type='text/javascript'>var currentposturl=&quot;<data:post.url/>&quot;;var maxresults=8;var relatedpoststitle=&quot;<b>Related Posts:</b>&quot;;removeRelatedDuplicates_thumbs();printRelatedLabels_thumbs();</script>
