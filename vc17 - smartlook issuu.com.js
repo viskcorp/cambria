8501d2df2b45664f8dae3902332dcdfac166adbf
viskcorp <viskcorp@gmail.com>
2015-03-17 15:27:27 +0700

    Create vc17 - smartlook issuu.com.js

---
+++ vc17 - smartlook issuu.com.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+document.write(unescape(&quot;%3Cscript src=&#39;http://static.issuu.com/smartlook/ISSUU.smartlook.js&#39; type=&#39;text/javascript&#39;%3E%3C/script%3E&quot;)); 
+//]]>
+</script>
