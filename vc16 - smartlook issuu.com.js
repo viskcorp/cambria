ddc8fed10f5271ae570f0c4f95a4e748fc7b867a
viskcorp <viskcorp@gmail.com>
2015-03-17 15:28:13 +0700

    Create vc16 - smartlook issuu.com.js

---
+++ vc16 - smartlook issuu.com.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+//<![CDATA[
+var issuuConfig={guid:&#39;2d223462-6058-4956-a525-2d20515ffd33&#39;,domain:&#39;*.viskcorp.blogspot.com&#39;};
+//]]>	
+</script>
+<script type='text/javascript'>
+//<![CDATA[
+document.write(unescape(&quot;%3Cscript src=&#39;http://static.issuu.com/smartlook/ISSUU.smartlook.js&#39; type=&#39;text/javascript&#39;%3E%3C/script%3E&quot;)); 
+//]]>
+</script>
