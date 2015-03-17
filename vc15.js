5350dab24a10662dfa7cbe55f869ce063b401abb
viskcorp <viskcorp@gmail.com>
2015-03-17 15:29:05 +0700

    Create vc15.js

---
+++ vc15.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+waitUntilExists(document,function(){Cufon.replace('#related-posts,#related-posts h2,#blog-pager-newer-link,#blog-pager-older-link,#blog-pager,.blank,.updateinfo,#page-footer,#nav-left,h1,h2,h3,h4,h5,h6,p,.post-body,.post blockquote,.postauthor,.postlabels,.social,.p-comment,.deleted-comment,.sidebar ul li,.sidebar h2,.footer-bot h2,.footer-bot ul,.footer-bot ul li,.showpageNum,.showpageOf,.showpagePoint,.sidebar-subscription,.rmlink',{hover:true});});
+//]]>
+</script>
