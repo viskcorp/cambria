d98e1ad3de1786ebe5ae0f7d13ad5dcd5aec1245
viskcorp <viskcorp@gmail.com>
2015-03-17 15:36:06 +0700

    Create vc8.js

---
+++ vc8.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+//<![CDATA[
+google.load(&quot;annotations&quot;,&quot;1&quot;,{&quot;locale&quot;:&quot;<data:top.languageCode/>&quot});function initialize(){google.annotations.setApplicationId(<data:top.blogspotReviews/>);google.annotations.createAll();google.annotations.fetch()}google.setOnLoadCallback(initialize);
+//]]>
+</script>
